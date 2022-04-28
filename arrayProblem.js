a6f678ee24847aa7d8e2ab45652ccaac912d2705
Kanchi27 <43461269+Kanchi27@users.noreply.github.com>
2022-04-28 12:04:23 +0530

    Create arrayProblem.js

---
+++ arrayProblem.js
@@ -0,0 +1,20 @@
+
+// what are the input arguments that foo funciton is called with , 
+// Return in order of foo's execution
+
+function foo(x){
+  console.log(x) 
+  // 0, undefined, 1, undefined, 2, 2, 3, 0
+  
+}
+
+const arr = new Array(1); // [<1* empty items>]
+arr.length  = 2; // [<2* empty items>]
+arr.push(2); // [ <2 empty items>, 2 ]
+arr.push(0);  // [ <2 empty items>, 2, 0 ]
+console.log(arr)
+for(const num in arr){
+  console.log('num',num) // 0,1,2,3 
+  foo(num);             // undefined, undefined, 2, 0
+  foo(arr[num]);
+}
