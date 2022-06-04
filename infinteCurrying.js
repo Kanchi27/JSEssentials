fc5cf4ec3b352a480dde988d8b10e3c53373a7b8
Kanchi27 <43461269+Kanchi27@users.noreply.github.com>
2022-06-04 19:03:31 +0530

    Update infinteCurrying.js

--- infinteCurrying.js
+++ infinteCurrying.js
@@ -10,3 +10,23 @@ const sum = (a) => (b) => b ? sum(a + b) : a
 console.log(sum(2)(3)(4)(5)(6)());
 
 console.log(sum1(2)(3)(4)(5)(6)());
+
+
+
+//  prob on currying
+function sum(...a){ // when you write a rest operator : a becomes an array , so ...a => is comma separated args whereas a is array
+  return function(...b){
+    if(!a.length) return 0;
+    if(!b.length)  {
+      return a.reduce((acc,curr) => acc+curr)
+    }
+    else return sum(...a,...b)  // 1,2,3 // 1,2,3,4
+  }
+}
+
+
+console.log(sum(1,2)(3)(4)());
+console.log(sum(12)(3)(4)());
+console.log(sum(1)());
+console.log(sum()());
+console.log(sum(0)());
