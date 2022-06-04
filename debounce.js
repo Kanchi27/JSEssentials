ef90c8c1f54d0fefd070fcfc1613b1179bd3062d
Kanchi27 <43461269+Kanchi27@users.noreply.github.com>
2022-06-04 17:21:22 +0530

    Update debounce.js

--- debounce.js
+++ debounce.js
@@ -1,3 +1,41 @@
+// https://www.telerik.com/blogs/debouncing-and-throttling-in-javascript
+// Throttling
+// is a technique in which, no matter how many times the user fires the event, the attached function will be executed only once
+// in a given time interval.
+
+
+// Debouncing
+// no matter how many times the user fires the event, the attached function will be executed only after the specified time
+// once the user stops firing the event.
+
+
+// The main difference between throttling and debouncing is that throttling executes the function at a regular interval, 
+// while debouncing executes the function only after some cooling period.
+
+// Use of Debouncing and Throttling in Real Life
+// We can throttle a button click event, if we do not want the user to spam by clicking the button frequently.
+// In the case of window resize event, if we want to redraw the UI only when the user has fixed the size of the window, we can use debouncing.
+// With Mouse move event, if we are displaying the location coordinates of the mouse pointer, it is much better to show the coordinates 
+// once the user reached the desired location. Here, we can use debouncing.
+
+
+const throttlePrint = (cb, d) => {
+    let timer;
+   
+    return function (...args) {
+      // If setTimeout is already scheduled, no need to do anything
+        if(timer)  return;
+      // Schedule a setTimeout after delay seconds
+      
+        timer = setTimeout(() => {
+        cb(...args);
+            // Once setTimeout function execution is finished, timerId = undefined so that in
+		    // the next event, function execution can be scheduled by the setTimeout
+        timer = undefined;
+        },d)
+    }
+}
+
 const debouncedPrint = (cb, d) => {
     let timer;
     return function (...args) {
@@ -5,7 +43,7 @@ const debouncedPrint = (cb, d) => {
         if (timer) clearTimeout(timer);
         // whenever user types , reinitiate the timer
         // when user stops typing, prev timers will be cancelled, current timer will complete and cb will be executed
-        setTimeout(() => {
+        timer = setTimeout(() => {
             cb(...args);
         }, d)
     }
